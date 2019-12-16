69baf681bab9544af92f297fa9be39a20614893f
olive <daniellek@mobilefuse.com>
2019-12-15 19:38:07 -0500

    function on customer

--- bamazoncust.js
+++ bamazoncust.js
@@ -18,19 +18,17 @@ let connection = mysql.createConnection({
 });
 //call on connection for mySQL and iinquirer node package allowing the user to interact via terminal and node
 connection.connect();
-
 start()
 
 function start (){
-    connection.query('SELECT * FROM products', function (error, res) {
+    connection.query('SELECT * FROM products', function (error, resp) {
         if (error) throw error;
-        // console.log(res);
-        res.forEach(row => {
-            console.log(`Id: ${row.id} Name: ${row.product_name} Price: $${row.price.toFixed(2)}\n` )
+        // console.log(resp);
+        resp.forEach(row => {
+            console.log(`Id: ${row.id} Name: ${row.product_name} Price: $${row.price.toFixed(2)} Quantity: ${row.stock}\n`)
         });
         askQuestions()
 
-        // connection.end()
     })
 }
 
@@ -45,42 +43,45 @@ function askQuestions() {
             message: "how many of this item would you like to purchase",
             type: "input",
             name: "prodQty"
-        }
-    ]).then(function (ans) {
-        let prodId = ans.prodId;
-        let prodQty = ans.prodQty;
-        // let allProd = getAllProd();
+          },
+        ])
+        .then(function (answers){
+        let prodId = answers.prodId;
+        let prodQty = answers.prodQty;
         withdrawProd(prodId, prodQty)
     });
-}
+};
 
 function withdrawProd(prodId, prodQty) {
-  connection.query('SELECT * FROM products', function (error, res) {
+  connection.query('SELECT * FROM products', function (error, resp) {
     if (error) throw error;
     let prod;
-    // console.log(res);
+    // console.log(resp);
     for (let i = 0; i < resp.length; i++) {
-      if (resp[i].id == ans.prodId) {
+      if (resp[i].id == prodId) {
         prod = resp[i]
       }
     }
     console.log(prod, "prod was found")
-      if(prod.stock_quantity >= prodQty){
-        orderComplete(prod, prodId, prodQty)
+      if(prod.stock >= prodQty){
+        orderComplete(prod, prodId, prodQty);
         connection.end()
-      }else{
+      }
+      else{
         console.log("sorry the order has been cancled, there was insuffecent stock of this purchase")
         connection.end()
       }
   })
 };
-function orderComplete (prodObj, prodId, prodQty) {
-  let newQuantity = prodObj.stock_quantity - prodQty;
-  let productSales = prodObj.price * prodQty;
-  let queryOne = "UPDATE products SET stock_quantity = ? where ?";
-  let queryTwo = "UPDATE products SET product_sales = ? where ?";
-  connection.query(queryOne,[newQuantity, {item_id: prodId}], function (error, res) {
+function orderComplete (prod, prodId, prodQty) {
+  let newQuantity = prod.stock - prodQty;
+  let productSales = prod.price * prodQty;
+  let queryOne = "UPDATE products SET stock = ? WHERE ?";
+  let queryTwo = "UPDATE products SET total_sales = ? WHERE ?";
+
+  connection.query(queryOne,[newQuantity, {id: prodId}], function (error, resp) {
   })
-  connection.query(queryTwo, [productSales, { item_id: prodId }], function (error, res) {
+  connection.query(queryTwo, [productSales, {id: prod}], function (error, resp) {
   })
+
 }
